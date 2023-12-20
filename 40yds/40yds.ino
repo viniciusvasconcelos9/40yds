98382b7c5c31fd5b7098e0162d573e53787534bf
Vinicius Vasconcelos <63874530+viniciusvasconcelos9@users.noreply.github.com>
2023-12-19 23:44:40 -0300

    final

--- 40yds/40yds.ino
+++ 40yds/40yds.ino
@@ -1,114 +1,122 @@
-#include "Ultrasonic.h" //INCLUSÃO DA BIBLIOTECA NECESSÁRIA PARA FUNCIONAMENTO DO CÓDIGO
+//Carrega a biblioteca do sensor ultrassonico e LCD
+#include <Ultrasonic.h>
+#include <LiquidCrystal.h>
+ 
+//Define os pinos para o trigger e echo
+#define pino_trigger1 6
+#define pino_echo1 7
+#define pino_trigger2 8
+#define pino_echo2 9
+ 
+//Inicializa o sensor nos pinos definidos acima e inicializa o LCD
+Ultrasonic ultrasonic1(pino_trigger1, pino_echo1);
+Ultrasonic ultrasonic2(pino_trigger2, pino_echo2); 
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
-const int echoPin = 7; //PINO DIGITAL UTILIZADO PELO HC-SR04 ECHO(RECEBE)
-const int trigPin = 6; //PINO DIGITAL UTILIZADO PELO HC-SR04 TRIG(ENVIA)
-int teste=0;
-int contagem=0;
-int inicio=0;
-int distancia=300;
-float tempoInicial=0;
-float tempoFinal=0;
-float resultado=0;
-int led1=3;
-int led2=4;
-int led3=5;
 
-Ultrasonic ultrasonic(trigPin,echoPin); //INICIALIZANDO OS PINOS DO ARDUINO
+void setup()
+{
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+  //Serial.println("Lendo dados do sensor...");
 
-int dist; //VARIÁVEL DO TIPO INTEIRO
-String result; //VARIÁVEL DO TIPO STRING
-
-void setup(){
-  
-  pinMode(led1, OUTPUT);
-  digitalWrite(led1, LOW);
-  pinMode(led2, OUTPUT);
-  digitalWrite(led2, LOW);
-  pinMode(led3, OUTPUT);
-  digitalWrite(led3, LOW);
-  
-  pinMode(echoPin, INPUT); //DEFINE O PINO COMO ENTRADA (RECEBE)
-  pinMode(trigPin, OUTPUT); //DEFINE O PINO COMO SAIDA (ENVIA)
-  Serial.begin(9600); //INICIALIZA A PORTA SERIAL
-  }
-void loop(){
-  
-   // FAZ A CHAMADA DO MÉTODO "hcsr04()"
-  //Serial.print("Distancia "); //IMPRIME O TEXTO NO MONITOR SERIAL
-  //Serial.print(result); ////IMPRIME NO MONITOR SERIAL A DISTÂNCIA MEDIDA
-  //Serial.println("cm"); //IMPRIME O TEXTO NO MONITOR SERIAL
-
-  
-  while(teste<10){
-    Serial.println("Iniciando...");
-    hcsr04();
-    distancia=result.toInt();
-    teste++;
-    digitalWrite(led1, HIGH);
-    digitalWrite(led2, HIGH);
-    digitalWrite(led3, HIGH);
-    delay(500);
-  }
-  while(teste==10){
-    
-    while(inicio==0){
-      digitalWrite(led3, LOW);
-      hcsr04();
-      distancia=result.toInt();
-      if(distancia<30){
-        Serial.println("Alinhado");
-        digitalWrite(led1, LOW);
-        digitalWrite(led2, HIGH);
-        
-        contagem++; 
-      }else{
-        Serial.println("Alinhar para iniciar");
-        digitalWrite(led1, HIGH);
-        digitalWrite(led2, LOW);
-        contagem=0;
-      }
-      if(contagem>10){
-        inicio=1;
-      }
-    delay(200);  
-    break;  
-    }
-    while(inicio==1){
-      hcsr04();
-      distancia=result.toInt();
-      if(distancia<30){
-        digitalWrite(led1, LOW);
-        digitalWrite(led2, LOW);
-        digitalWrite(led3, HIGH);
-        Serial.println("Pronto");
-      }
-      if(distancia>100){
-        digitalWrite(led3, LOW);
-        Serial.println("Start!!");
-        tempoInicial=millis();
-        inicio=2;    
-      }
-      break;
-    }
-    while(inicio==2){
-      tempoFinal=millis();
-      resultado=(tempoFinal-tempoInicial)/1000;
-      Serial.println(resultado,3);
-      break;
-    }
-  }
+  lcd.clear();
+  lcd.setCursor(3, 0);
+  lcd.print("Iniciando..");
+  delay(3000);
+  //lcd.clear();
 }
-//MÉTODO RESPONSÁVEL POR CALCULAR A DISTÂNCIA
-void hcsr04(){
-    digitalWrite(trigPin, LOW); //SETA O PINO 6 COM UM PULSO BAIXO "LOW"
-    delayMicroseconds(2); //INTERVALO DE 2 MICROSSEGUNDOS
-    digitalWrite(trigPin, HIGH); //SETA O PINO 6 COM PULSO ALTO "HIGH"
-    delayMicroseconds(10); //INTERVALO DE 10 MICROSSEGUNDOS
-    digitalWrite(trigPin, LOW); //SETA O PINO 6 COM PULSO BAIXO "LOW" NOVAMENTE
-    //FUNÇÃO RANGING, FAZ A CONVERSÃO DO TEMPO DE
-    //RESPOSTA DO ECHO EM CENTIMETROS, E ARMAZENA
-    //NA VARIAVEL "distancia"
-    dist = (ultrasonic.Ranging(CM)); //VARIÁVEL GLOBAL RECEBE O VALOR DA DISTÂNCIA MEDIDA
-    result = String(dist); //VARIÁVEL GLOBAL DO TIPO STRING RECEBE A DISTÂNCIA(CONVERTIDO DE INTEIRO PARA STRING)
-    delay(5); //INTERVALO DE 500 MILISSEGUNDOS
- }
+
+int teste, contagem;
+float sen1, sen2, distancia, tempoInicial, tempo;
+int select=0;
+
+
+void loop (){
+    if (select==0){
+      yds40();
+    }
+  
+}
+
+void yds40(){
+
+  
+      contagem = 0;
+      teste=0;
+      
+      while(teste==0){
+         while(contagem<10){ 
+            distancia = sensor_1(); 
+            if(distancia<50){
+              lcd.clear();
+              lcd.setCursor(1, 0);
+              lcd.print("20/40 yds dash");
+              lcd.setCursor(2, 1);
+              lcd.print("- Alinhando -");
+              //Serial.println("Alinhado");     
+              contagem++;
+              if (contagem==10){
+                lcd.clear();
+                lcd.setCursor(1, 0);
+                lcd.print("20/40 yds dash");
+                lcd.setCursor(3, 1);
+                lcd.print("- Pronto -");   
+              }
+            }else{
+              lcd.clear();
+              lcd.setCursor(1, 0);
+              lcd.print("20/40 yds dash");
+              lcd.setCursor(2, 1);
+              lcd.print("- Preparar -");
+              //Serial.println("Alinhar para iniciar");
+              contagem=0;
+            }
+            delay(300); 
+          }
+       
+        distancia = sensor_1(); 
+          
+        if (distancia>20){
+          lcd.clear();
+          lcd.setCursor(1, 0);
+          lcd.print("20/40 yds dash");
+          lcd.setCursor(3, 1);
+          lcd.print("---------");
+          //Serial.println("Inicio");
+          teste=1;
+          tempoInicial=millis();
+        }  
+     }
+     while(teste==1){
+        distancia = sensor_2();
+        if (distancia<100){
+          tempo = millis() - tempoInicial;
+          lcd.clear();
+          lcd.setCursor(1, 0);
+          lcd.print("- Tempo Final -");
+          lcd.setCursor(6, 1);
+          lcd.print(tempo/1000);
+          delay(5000);
+          break;
+        }
+     }
+}
+
+float sensor_1()
+{
+  //Le as informacoes do sensor, em cm e pol
+  float cmMsec;
+  long microsec = ultrasonic1.timing();
+  cmMsec = ultrasonic1.convert(microsec, Ultrasonic::CM);
+  return cmMsec;
+}
+
+float sensor_2()
+{
+  //Le as informacoes do sensor, em cm e pol
+  float cmMsec;
+  long microsec = ultrasonic2.timing();
+  cmMsec = ultrasonic2.convert(microsec, Ultrasonic::CM);
+  return cmMsec;
+}
